919f4d9d6f2c655c995c91d7b5aae3d4e5b9779b
TheWebDevel <sathish@skcript.com>
2020-06-24 00:05:03 +0530

    resolved fmt warning

--- tests/capture_codeframe/macro_test.rs
+++ tests/capture_codeframe/macro_test.rs
@@ -16,7 +16,7 @@ fn with_context() {
     if let Some(_codeframe) = &codeframe {}
     assert_matches_inline_snapshot!(
         format!("\n{}", &codeframe.expect("must be present")),
-         "
+        "
 [2m13 | fn with_context() {[0m
 [2m14 |     super::setup_test_env();[0m
 [34m15 |     let codeframe = get_codeframe!();[0m
