8dad67e6cf2a193a90616d2e0ce121b9e8824a3d
TheWebDevel <sathish@skcript.com>
2020-06-23 23:54:57 +0530

    updated test cases

--- tests/macro_test.rs
+++ tests/macro_test.rs
@@ -12,17 +12,15 @@ macro_rules! get_codeframe {
 #[test]
 fn with_context() {
     let codeframe = get_codeframe!();
-    if let Some(codeframe) = &codeframe {
-        println!("{}", codeframe);
-    }
+    if let Some(_codeframe) = &codeframe {}
     assert_matches_inline_snapshot!(
-        format!("\n{}", codeframe.expect("must be present")),
-        "
+        format!("\n{}", &codeframe.expect("must be present")),
+         "
 [2m12 | #[test][0m
 [2m13 | fn with_context() {[0m
 [34m14 |     let codeframe = get_codeframe!();[0m
-[2m15 |     if let Some(codeframe) = &codeframe {[0m
-[2m16 |         println!(\"{}\", codeframe);[0m
+[2m15 |     if let Some(_codeframe) = &codeframe {}[0m
+[2m16 |     assert_matches_inline_snapshot!([0m
 "
     );
 }
