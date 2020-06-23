bf9c024c0ebbcfaa8c33b6561d09f8a810148f26
TheWebDevel <sathish@skcript.com>
2020-06-24 01:45:38 +0530

    better test cases

--- tests/capture_codeframe/builder_test.rs
+++ tests/capture_codeframe/builder_test.rs
@@ -40,6 +40,33 @@ fn simple_capture() {
 [31m8 |             )[0m
 "
     );
+
+    let raw_lines = "macro_rules! test_simple_style {
+    ($string:expr, $style:ident) => {
+        #[test]
+        fn $style() {
+            assert_eq!(
+                s.$style().to_string(),
+                ansi_term::Style::new().$style().paint(s).to_string()
+            )
+        }
+    };
+}"
+    .to_owned();
+
+    let codeframe = Codeframe::new(raw_lines, 5).capture();
+
+    assert_matches_inline_snapshot!(
+        format!("\n{}", codeframe.expect("must be present")),
+        "
+[2m3 |         #[test][0m
+[2m4 |         fn $style() {[0m
+[31m5 |             assert_eq!([0m
+[31m6 |                 s.$style().to_string(),[0m
+[31m7 |                 ansi_term::Style::new().$style().paint(s).to_string()[0m
+[31m8 |             )[0m
+"
+    );
 }
 
 #[test]
--- tests/capture_codeframe/macro_test.rs
+++ tests/capture_codeframe/macro_test.rs
@@ -9,19 +9,43 @@ macro_rules! get_codeframe {
     }};
 }
 
+macro_rules! get_codeframe_with_default_color {
+    () => {{
+        let codeframe = capture_codeframe!();
+        codeframe
+    }};
+}
+
 #[test]
-fn with_context() {
+fn with_color() {
     super::setup_test_env();
     let codeframe = get_codeframe!();
     if let Some(_codeframe) = &codeframe {}
     assert_matches_inline_snapshot!(
         format!("\n{}", &codeframe.expect("must be present")),
-        "
-[2m13 | fn with_context() {[0m
-[2m14 |     super::setup_test_env();[0m
-[34m15 |     let codeframe = get_codeframe!();[0m
-[2m16 |     if let Some(_codeframe) = &codeframe {}[0m
-[2m17 |     assert_matches_inline_snapshot!([0m
+         "
+[2m20 | fn with_color() {[0m
+[2m21 |     super::setup_test_env();[0m
+[34m22 |     let codeframe = get_codeframe!();[0m
+[2m23 |     if let Some(_codeframe) = &codeframe {}[0m
+[2m24 |     assert_matches_inline_snapshot!([0m
+"
+    );
+}
+
+#[test]
+fn without_color() {
+    super::setup_test_env();
+    let codeframe = get_codeframe_with_default_color!();
+    if let Some(_codeframe) = &codeframe {}
+    assert_matches_inline_snapshot!(
+        format!("\n{}", &codeframe.expect("must be present")),
+         "
+[2m37 | fn without_color() {[0m
+[2m38 |     super::setup_test_env();[0m
+[31m39 |     let codeframe = get_codeframe_with_default_color!();[0m
+[2m40 |     if let Some(_codeframe) = &codeframe {}[0m
+[2m41 |     assert_matches_inline_snapshot!([0m
 "
     );
 }
